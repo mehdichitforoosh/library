edc3e7df77ebd42e784da5ba676d9ebb94e6026f
mehdichitforoosh <mehdichitforoosh@gmail.com>
2018-09-21 21:42:10 +0430

    Add uikit and import uikit in index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import uikitcss from 'uikit/dist/css/uikit-rtl.min.css'
+import ukitjs from 'uikit/dist/js/uikit'
+import uikiticonsjs from 'uikit/dist/js/uikit-icons'
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+        <App />
+    </Router>,
+    document.getElementById('root'));
