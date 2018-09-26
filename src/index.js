b25a984158effc6512d102381980288d66906a99
mehdichitforoosh <mehdichitforoosh@gmail.com>
2018-09-26 14:29:39 +0330

    Add persian font vazir-font

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import uikitcss from 'uikit/dist/css/uikit-rtl.min.css'
 import ukitjs from 'uikit/dist/js/uikit'
 import uikiticonsjs from 'uikit/dist/js/uikit-icons'
+import fonts from "vazir-font/dist/font-face.css";
+import styles from './styles.css'
 import App from './App';
 
 ReactDOM.render(
